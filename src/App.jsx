a0345641d83bd39e71a07d76f469f2f11524a2c0
HPDJOY <75718586+Hpdjoy@users.noreply.github.com>
2025-07-21 20:31:40 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+        <h1>HPDJOY<h1/>
         </p>
       </div>
       <p className="read-the-docs">
